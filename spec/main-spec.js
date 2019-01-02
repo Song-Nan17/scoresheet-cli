31b281ee5a09b758b91cd50676585b8324863677
Song-Nan17 <songnan17@qq.com>
2019-01-02 18:48:20 +0800

    Write main and add computeAverage & computeMedian.

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -4,18 +4,6 @@ let Student = require("../lib/student.js");
 let addStudent = require("../lib/addStudent");
 let generateTranscript = require("../lib/generateTranscript");
 
-// describe('main()', () => {
-
-//     it('should display main menu once started', () => {
-//         sinon.spy(console, 'log');
-//         main();
-//         expect(console.log.args.join()).toBe(`1. 添加学生
-// 2. 生成成绩单
-// 3. 退出
-// 请输入你的选择（1～3）：`);
-//     });
-
-// });
 
 describe('addStudent()', () => {
     let studentInfo = '张三，101，数学：95，语文：92，英语：93，编程：94';
@@ -34,10 +22,11 @@ describe('addStudent()', () => {
 describe('generateTranscript()', () => {
     let info = '101，102';
     let errorInfo = '101，asd';
-    let inexistentInfo = '101，102，103';
+    let inexistentInfo = '101，102，104';
     let student = new Student('张三', 101, { math: 95, chinese: 92, english: 93, coding: 94 });
     let otherStudent = new Student('李四', 102, { math: 90, chinese: 97, english: 91, coding: 89 });
-    let students = [student, otherStudent];
+    let thirdStudent = new Student('王五', 103, { math: 89, chinese: 92, english: 94, coding: 88 });
+    let students = [student, otherStudent, thirdStudent];
     it('should generate transcript while input is correct', () => {
         let result = generateTranscript(info, students);
         expect(result).toBe(`成绩单
@@ -46,8 +35,8 @@ describe('generateTranscript()', () => {
 张三|95|92|93|94|93.5|374
 李四|90|97|91|89|91.75|367
 ========================
-全班总分平均数：xxx
-全班总分中位数：xxx
+全班总分平均数：368
+全班总分中位数：367
 `);
     });
     it('should ignore student which is inexistent', () => {
@@ -58,8 +47,8 @@ describe('generateTranscript()', () => {
 张三|95|92|93|94|93.5|374
 李四|90|97|91|89|91.75|367
 ========================
-全班总分平均数：xxx
-全班总分中位数：xxx
+全班总分平均数：368
+全班总分中位数：367
 `);
     });
     it('should return -1 while input info is not current', () => {
