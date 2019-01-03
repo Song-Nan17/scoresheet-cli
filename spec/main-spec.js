1c44671301dc46f904e2c0d453fe1c09c60468e9
Song-Nan17 <songnan17@qq.com>
2019-01-03 13:18:05 +0800

    Modify Student.courses and take out functions in generateTransctipt.

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,20 +1,20 @@
 let sinon = require("sinon");
 let main = require("../lib/main");
 let Student = require("../lib/student.js");
-let addStudent = require("../lib/addStudent");
+let generateNewStudent = require("../lib/generateNewStudent");
 let generateTranscript = require("../lib/generateTranscript");
 
 
-describe('addStudent()', () => {
+describe('generateNewStudent()', () => {
     let studentInfo = '张三，101，数学：95，语文：92，英语：93，编程：94';
     let errorStudentInfo = '张三，101，数学：95，语文：qw，英语：93，编程：94';
     it('should get student while input studentInfo is correct', () => {
-        let result = addStudent(studentInfo);
-        let student = new Student('张三', 101, { math: 95, chinese: 92, english: 93, coding: 94 });
+        let result = generateNewStudent(studentInfo);
+        let student = new Student('张三', 101, [{ course: '数学', score: 95 }, { course: '语文', score: 92 }, { course: '英语', score: 93 }, { course: '编程', score: 94 }]);
         expect(result).toEqual(student)
     });
     it('should return -1 while input studentInfo is not correct', () => {
-        let result = addStudent(errorStudentInfo);
+        let result = generateNewStudent(errorStudentInfo);
         expect(result).toEqual(-1);
     });
 });
@@ -23,9 +23,9 @@ describe('generateTranscript()', () => {
     let info = '101，102';
     let errorInfo = '101，asd';
     let inexistentInfo = '101，102，104';
-    let student = new Student('张三', 101, { math: 95, chinese: 92, english: 93, coding: 94 });
-    let otherStudent = new Student('李四', 102, { math: 90, chinese: 97, english: 91, coding: 89 });
-    let thirdStudent = new Student('王五', 103, { math: 89, chinese: 92, english: 94, coding: 88 });
+    let student = new Student('张三', 101, [{ course: '数学', score: 95 }, { course: '语文', score: 92 }, { course: '英语', score: 93 }, { course: '编程', score: 94 }]);
+    let otherStudent = new Student('李四', 102, [{ course: '数学', score: 90 }, { course: '语文', score: 97 }, { course: '英语', score: 91 }, { course: '编程', score: 89 }]);
+    let thirdStudent = new Student('王五', 103, [{ course: '数学', score: 89 }, { course: '语文', score: 92 }, { course: '英语', score: 94 }, { course: '编程', score: 88 }]);
     let students = [student, otherStudent, thirdStudent];
     it('should generate transcript while input is correct', () => {
         let result = generateTranscript(info, students);
